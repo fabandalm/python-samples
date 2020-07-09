ecb062b38eac9f10fa9870383ef9748acd831b9f
Almeida <falmeida.tech@gmail.com>
2020-07-08 22:53:34 -0400

    Added decorator

---
+++ decorator_sample01.py
@@ -0,0 +1,13 @@
+def decorator_function(original_function):
+	def wrapper_function():
+		print('wrapper executed this before {}'.format(original_function.__name__))
+		return original_function()
+	return wrapper_function
+
+@decorator_function
+def display():
+	print('display function ran')
+
+display()
+
+	
