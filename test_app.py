5670a05f3229b29e415776b3e112220bb5ea4d1e
Fabio Almeida <falmeida.tech@gmail.com>
2020-07-24 23:39:16 -0400

    Upgraded the test_app

--- test_app.py
+++ test_app.py
@@ -6,6 +6,12 @@ class TestFibonacci(unittest.TestCase):
     def test_fibonacci_1(self):
         self.assertEqual(fibonacci(1),1)
 
+    def test_fibonacci_10(self):
+        self.assertEqual(fibonacci(10),89)
+
+    def test_fibonacci_30(self):
+        self.assertEqual(fibonacci(30),1346269)    
+
 if __name__ == '__main__':
     unittest.main()
 
