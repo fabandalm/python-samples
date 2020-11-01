f1f02d5478e3526e685988490719030497e60a6f
Fabio Almeida <falmeida.tech@gmail.com>
2020-10-31 22:00:08 -0400

    new function for default argument

--- hw_start/app.py
+++ hw_start/app.py
@@ -2,4 +2,10 @@ def greet(first_name, last_name):
     print(f"Hi {first_name} {last_name}")
 
 
+def increment(number, by=1):
+    return number + by
+
+
 greet("John", 'Smith')
+result = increment(7)
+print(result)
