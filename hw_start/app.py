c779a93568a546ce35c1da9b3c2b6f653512e4fc
Fabio Almeida <falmeida.tech@gmail.com>
2020-11-02 21:49:35 -0500

    Added new function * and **

--- hw_start/app.py
+++ hw_start/app.py
@@ -5,6 +5,7 @@ def greet(first_name, last_name):
 def increment(number, by=1):
     return number + by
 
+
 def multiply(*numbers):
     total = 1
     for number in numbers:
@@ -12,7 +13,16 @@ def multiply(*numbers):
     return total
 
 
+def save_user(**user):
+    print(user)
+    print(user["id"])
+
+
 greet("John", 'Smith')
+
 result = increment(7)
 print(result)
 print(multiply(1,2,3,4,5))
+
+save_user(id=1,name="John")
+
